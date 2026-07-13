de6fe77bf475efd10de179ab6e70440aa42d891d
Tomohiro C. Yoshida <tomohiroyoshida.astro@gmail.com>
2026-07-13 17:00:00 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 from setuptools import setup
+from pathlib import Path
 
-
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="FRAP",
@@ -8,6 +10,8 @@ setup(
     author="Tomohiro C. Yoshida",
     author_email="tomohiroyoshida.astro@gmail.com",
     description="Flexible Radial Analysis of Protoplanetary disks",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     packages=["frap"],
     project_urls={
         "Documentation": "https://frap.readthedocs.io/en/latest/index.html",
