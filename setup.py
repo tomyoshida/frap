e8cb82ab500013f90646ac3e5a8d9337faf9c1c4
Tomohiro C. Yoshida <tomohiroyoshida.astro@gmail.com>
2026-07-13 17:13:56 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,8 @@ setup(
     version="1.0.0.post02",
     author="Tomohiro C. Yoshida",
     author_email="tomohiroyoshida.astro@gmail.com",
-    description="Flexible Radial Analysis of Protoplanetary disks. A Python module for astronomical data analysis.",
+    description="Flexible Radial Analysis of Protoplanetary disks.",
+    long_description="This is a Python module for astronomical data analysis. See our [documentation](https://frap.readthedocs.io/en/latest/index.html) for details.",
     packages=["frap"],
     project_urls={
         "Documentation": "https://frap.readthedocs.io/en/latest/index.html",
